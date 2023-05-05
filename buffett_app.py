1bc5f165ff9eb89e8db7f380c8ad10c11078f4ec
Randy Pettus <jrpettus@gmail.com>
2023-05-05 16:35:41 -0600

    updated app with significant changes

--- buffett_app.py
+++ buffett_app.py
@@ -66,6 +66,7 @@ Various tickers are all included in a single table for each of the three core fi
 INCOME_STATEMENT_ANNUAL (alias: isa), 
 CASH_FLOW_STATEMENT_ANNUAL (alisas: cfs), 
 BALANCE_SHEET_ANNUAL (alias: bsa)
+Only use the alias after using the full table name and only include the relevant financial statements.
 Make sure any request is translated to one or more of these specific table names including the "_ANNUAL" suffix.
 Column names do not have a space and they do not have an underscore so you will need to translate a request for'Net income' to something like netincome or free cash flow to freecashflow
 In the event that a request is made that would come from multiple financial statements, then join based on the year and ticker columns. For instance, if the netincome and assets are necessary take netincome from the income statement and assets from the balance sheet statement and join based on year.
@@ -109,33 +110,144 @@ tick_list = ['BRK.A','AAPL','PG','JNJ','MA','MCO','VZ','KO','AXP', 'BAC']
 fin_statement_list = ['income_statement','balance_sheet','cash_flow_statement']
 
 # create tabs
-tab1, tab2, tab3 = st.tabs(["Data Exploration", "LLM-Powered Q/A ", "Model Building"])
+tab1, tab2, tab3, tab4 = st.tabs(["Financial Data Exploration :chart_with_upwards_trend:", "Financial Statement Natural Language Querying :dollar:", 
+                                  "Shareholder Letter Natural Language Querying :memo:", "Additional Details :notebook:"])
+
+with st.sidebar:
+    st.markdown("""
+    # View into Warren Buffett :moneybag:
+    This app enables various exploration into the World of Warren Buffett, including financials for 
+    large language models (LLMs) leveraging Snowflake :snowflake:, Streamlit, OpenAI, Langchain and Pinecone to dive 
+
+    Tabs:
+
+    ## Tab1: Financial Data Exploration :chart_with_upwards_trend: : 
+    Query Snowflake to view financials for various Warren Buffett investments going back 30 years by querying Snowflake directly
+    ## Tab2: Financial Statement Natural Language Querying :dollar:
+    Ask financial questions using natural language regarding the investments
+    ## Tab3: Shareholder Letter Natural Language Querying :memo:
+    Ask various questions based on Warren Buffett's shareholder letters from 1977 through 2022  
+
+    **Current Available Companies to ask financials about for tabs 1 and 2:**
+    1. Apple
+    2. Bershire Hathaway
+    3. Proctor and Gamble
+    4. Johnson and Johnson
+    5. Mastercard
+    6. Moodys Corp
+    7. Verizon
+    8. American Express
+    9. Bank of America  
+    """)
 
 with tab1:
+    st.markdown("""
+    # Financials :chart_with_upwards_trend:
+
+    View financial statement data for selected companies owned by Warren Buffet by querying Snowflake directly.
+
+    Available companies are identified in the selection drop down
+    """)
     sel_tick = st.selectbox("Select a ticker to view", tick_list)
-    sel_statement = st.selectbox("Select a ticker to view", fin_statement_list)
-    df = sf_query(f"select * from financials.public.{sel_statement}_annual where ticker = '{sel_tick}'")
-    st.dataframe(df)
+
+    inc_st = sf_query(f"select * from financials.public.income_statement_annual where ticker = '{sel_tick}'")
+    bal_st = sf_query(f"select * from financials.public.balance_sheet_annual where ticker = '{sel_tick}'")
+    cf_st = sf_query(f"select * from financials.public.cash_flow_statement_annual where ticker = '{sel_tick}'")
+
+    # metrics for kpi cards
+    def kpi_recent(df, metric, periods=2, unit=1000000000):
+        return df.sort_values('year',ascending=False).head(periods)[metric]/unit
+    
+    # find the most recent 2 periods
+    net_inc = kpi_recent(inc_st, 'netincome')
+    net_inc_rat = kpi_recent(inc_st, 'netincomeratio', periods=2, unit=1)
+    fcf = kpi_recent(cf_st, 'freecashflow' )
+  
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.subheader("Net Income")
+        st.metric('', f'${net_inc[0]}B', delta=round(net_inc[0]-net_inc[1],2), delta_color="normal", help=None, label_visibility="visible")
+        st.altair_chart(alt.Chart(inc_st).mark_bar().encode(
+            x='year',
+            y='netincome'
+            ).properties(title="Net Income")
+        ) 
+    
+    with col2:
+        st.subheader("Net Profit Margin")
+        # netincome ratio
+        st.metric('', f'{round(net_inc_rat[0]*100,2)}%', delta=round(net_inc_rat[0]-net_inc_rat[1],2), delta_color="normal", help=None, label_visibility="visible")
+        st.altair_chart(alt.Chart(inc_st).mark_bar().encode(
+            x='year',
+            y='netincomeratio'
+            ).properties(title="Net Profit Margin")
+        ) 
+
+    with col3:
+        st.subheader("Free Cashflow")
+        # free cashflow
+        st.metric('', f'${fcf[0]}B', delta=round(fcf[0]-fcf[1],2), delta_color="normal", help=None, label_visibility="visible")
+        st.altair_chart(alt.Chart(cf_st).mark_bar().encode(
+            x='year',
+            y='freecashflow'
+            ).properties(title="Free Cash Flow")
+        ) 
+
+    sel_statement = st.selectbox("Select a statement to view", fin_statement_list)
+    fin_statement_dict = {'income_statement': inc_st, 'balance_sheet': bal_st, 'cash_flow_statement':cf_st}
+    st.dataframe(fin_statement_dict[sel_statement])
 
 with tab2:
-    llm = llm_connection(temperature=0 )
+    st.markdown("""
+    # Natural Language Financials Querying :dollar:
+    ### Leverage LLMs to translate natural language questions related to financial statements and turn those into direct Snowflake queries
+    Data is stored and queried directly from income statement, balance sheet, and cash flow statement in Snowflake
+
+    **Example questions to ask:**
+
+    - What was the net income in 1996 through 2000 for Proctor and Gamble?
+    - What was the revenue vs. total assets for Apple for the last 5 years?
+    """
+    )
+
+    llm = llm_connection(temperature=0, )
     str_input = st.text_input(label='What would you like to answer? (e.g. What was the revenue and net income for Apple for the last 5 years?)')
 
     if len(str_input) > 1:
-        output = db_chain(str_input)
-        st.write(output['result'])
-        st.write(sf_query(output['intermediate_steps'][0]))
+        try:
+            output = db_chain(str_input)
+            st.write(output['result'])
+            st.write(sf_query(output['intermediate_steps'][0]))
+        except:
+            st.write("Please try to improve your prompt or provide feedback on the error encountered")
 
-        llm(f"Based on the output provided in {output['result']} produce python code to turn the data into a dataframe and plot the results but skip the import steps")
+        #llm(f"Based on the output provided in {output['result']} produce python code to turn the data into a dataframe and plot the results but skip the import steps")
         #agent_executor.run(f"Based on the output provided in {output['result']}, create a dataframe of the contents and plot the results with an informative title")
 
-    agent_executor = create_python_agent(
-        llm=llm,
-        tool=PythonREPLTool(),
-        verbose=False
-    )
+    #agent_executor = create_python_agent(
+    #    llm=llm,
+    #    tool=PythonREPLTool(),
+    #    verbose=False
+    #)
 
     with tab3:
+        st.markdown("""
+        # Shareholder Letter Natural Language Querying :memo:
+        ### Ask questions from all of Warren Buffett's annual shareholder letters dating back to 1977
+
+        These letters are much anticipated by investors for the wealth of knowledge that Buffett provides.
+        The tool allows you to interact with these letters by asking questions and a LLM is used to find relevant answers.
+
+        **Example questions to ask:**
+
+        - Why has Apple been a good investment? What specific reasons has it increased in value?
+        - What are some of your biggest lessons learned?
+        - What do you look for in managers?
+        - Are markets efficient? Give a specific example that you have used in a letter.
+        """
+        )
+
         pinecone_init()
         index_name = "buffett"
         embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["openai_key"])
@@ -144,13 +256,27 @@ with tab2:
 
         pdf_qa = ChatVectorDBChain.from_llm(OpenAI(temperature=0.1, model_name="gpt-3.5-turbo",openai_api_key=st.secrets["openai_key"]),
                             docsearch, return_source_documents=True)
+        
+        @st.cache_data(ttl=600)
+        def pdf_question(query):
+            return pdf_qa({"question": query, "chat_history": ""})
 
-        query = st.text_input("What would you like to ask Warren? (e.g List reasons you purchased Apple and why it has worked out so well?)")
+        query = st.text_input("What would you like to ask Warren Buffett?")
         if len(query)>1:
-            result = pdf_qa({"question": query, "chat_history": ""})
-            #print(result)
-            st.write(result['answer'])
-            st.markdown("Additional details from the search result")
-            st.write(result)
+            try:
+                result = pdf_question(query)
+                #print(result)
+                st.write(result['answer'])
+                #st.markdown("Additional details from the search result")
+                #st.write(result)
+            except:
+                st.write("Please try to improve your prompt or provide feedback on the error encountered")
 
+with tab4:
+    st.markdown("""
+    
+    Additional Details:
 
+    - Tabs 2 and 3 can likely be consolidated leveraging Langchain "tools" with better prompting templates.
+
+    """)
