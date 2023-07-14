0f2d2a4b842c067f533b5e33e17b43fc64c4786e
Randy Pettus <jrpettus@gmail.com>
2023-07-14 12:08:33 -0600

    changed back

--- buffett_app.py
+++ buffett_app.py
@@ -2,31 +2,96 @@ import snowflake.connector
 import numpy as np
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 import altair as alt
 
-import prompts # see github prompts.py
+from streamlit_chat import message
 
 from langchain.llms import OpenAI
 from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chains.llm_math.base import LLMMathChain
+from langchain.llms import OpenAI
+from langchain.agents import initialize_agent, Tool, load_tools
+from langchain import SQLDatabase, SQLDatabaseChain
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.tools.python.tool import PythonREPLTool
+from langchain.python import PythonREPL
 from langchain.embeddings import OpenAIEmbeddings # for creating embeddings
 from langchain.chains import ChatVectorDBChain # for chatting with the pdf
-from langchain.vectorstores import Pinecone # for the vectorization part
-
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.prompts.prompt import PromptTemplate
-from langchain.chains import (
-    RetrievalQA
-)
-#from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import Chroma, Pinecone # for the vectorization part
 
 import pinecone
 
+from sqlalchemy import create_engine
+from sqlalchemy.dialects import registry
+registry.register('snowflake', 'snowflake.sqlalchemy', 'dialect')
 
-sf_db = st.secrets["sf_database"]
-sf_schema = st.secrets["sf_schema"]
+sf_user = st.secrets["user"]
+sf_pw = st.secrets["password"]
+sf_acct = st.secrets["account"]
+sf_db = st.secrets["database"]
+sf_schema = st.secrets["schema"]
+sf_wh = st.secrets["warehouse"]
 
 st.set_page_config(layout="wide")
+
+# Uses st.cache_resource to only run once.
+#@st.cache_resource
+def llm_connection(temperature=0):
+    return OpenAI(temperature=temperature, openai_api_key=st.secrets["openai_key"] )
+
+#@st.cache_resource
+def sf_engine():
+    engine = create_engine(
+        f"snowflake://{sf_user}:{sf_pw}@{sf_acct}/{sf_db}/{sf_schema}?warehouse={sf_wh}"
+        )
+    return engine
+
+#@st.cache_resource
+def sf_connection():
+    return engine.connect()
+
+#@st.cache_resource
+def sql_db():
+    return SQLDatabase(engine)
+
+from langchain.prompts.prompt import PromptTemplate
+
+_DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct SNOWFLAKE query to run, then look at the results of the query and return the answer.
+You will be answering questions about financial statements. So only consider the appropriate financial statement that a question would relate.
+For instance, only use the income statement if a question is posed for revenue. 
+Various tickers are all included in a single table for each of the three core financial statements and are located in these tables with the alias to be used next to each table in parentheses
+INCOME_STATEMENT_ANNUAL (alias: isa), 
+CASH_FLOW_STATEMENT_ANNUAL (alisas: cfs), 
+BALANCE_SHEET_ANNUAL (alias: bsa)
+Only use the alias after using the full table name and only include the relevant financial statements.
+Make sure any request is translated to one or more of these specific table names including the "_ANNUAL" suffix.
+In the event that a request is made that would come from multiple financial statements, then join based on the year and ticker columns. For instance, if the net_income and assets are necessary take netincome from the income statement and assets from the balance sheet statement and join based on year.
+It is very important to create an alias for each table during the join and don't bring in year multiple times for the resulting output to avoid ambiguous names. Just choose one of the year columns instead
+A user may request total assets in which this means total_assets column and not to sum assets
+
+Use the following format:
+
+Question: "Question here"
+SQLQuery: "SQL Query to run"
+SQLResult: "Result of the SQLQuery"
+Answer: "Final answer here"
+
+Question: {input}"""
+PROMPT = PromptTemplate(
+    input_variables=["input"], template=_DEFAULT_TEMPLATE
+)
+
+#@st.cache_data(ttl=600)
+def db_chain(str_input):
+    sf_chain = SQLDatabaseChain(llm=llm, database=sql_database, prompt=PROMPT, return_intermediate_steps=True)
+    return sf_chain(str_input)
+
+#@st.cache_data(ttl=600)
+def sf_query(str_input):
+    return pd.read_sql(str_input, connection)
 
 #@st.cache_resource
 def pinecone_init():
@@ -35,24 +100,10 @@ def pinecone_init():
     environment=st.secrets['pinecone_env'] 
     )
 
-pinecone_init()
-index_name = "buffett"
-embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["openai_key"])
+engine = sf_engine()
+connection = sf_connection()
+sql_database = sql_db()
 
-docsearch = Pinecone.from_existing_index(index_name,embeddings)
-
-pdf_qa = ChatVectorDBChain.from_llm(OpenAI(temperature=0.1, model_name="gpt-3.5-turbo",openai_api_key=st.secrets["openai_key"]),
-                    docsearch, return_source_documents=True)
-
-@st.cache_data(ttl=600)
-def pull_financials(database, schema, statement, ticker):
-    df = conn.query(f"select * from {database}.{schema}.{statement} where ticker = '{ticker}' order by year desc")
-    df.columns = [col.lower() for col in df.columns]
-    return df
-
-#@st.cache_data(ttl=600)
-#def pdf_question(query):
-#    return pdf_qa({"question": query, "chat_history": ""})
 
 tick_list = ['BRK.A','AAPL','PG','JNJ','MA','MCO','VZ','KO','AXP', 'BAC']
 fin_statement_list = ['income_statement','balance_sheet','cash_flow_statement']
@@ -88,9 +139,7 @@ with st.sidebar:
     9. Bank of America  
     """)
 
-
 with tab1:
-    conn = st.experimental_connection("snowpark")
     st.markdown("""
     # Financial Data Exploration :chart_with_upwards_trend:
 
@@ -100,10 +149,10 @@ with tab1:
     """)
     sel_tick = st.selectbox("Select a ticker to view", tick_list)
 
-    inc_st = pull_financials(sf_db, sf_schema, 'income_statement_annual', sel_tick)
-    bal_st = pull_financials(sf_db, sf_schema, 'balance_sheet_annual', sel_tick)
+    inc_st = sf_query(f"select * from {sf_db}.{sf_schema}.income_statement_annual where ticker = '{sel_tick}' order by year desc")
+    bal_st = sf_query(f"select * from {sf_db}.{sf_schema}.balance_sheet_annual where ticker = '{sel_tick}' order by year desc")
     bal_st['debt_to_equity'] = bal_st['total_debt'].div(bal_st['total_equity'])
-    cf_st =  pull_financials(sf_db, sf_schema, 'cash_flow_statement_annual', sel_tick) 
+    cf_st = sf_query(f"select * from {sf_db}.{sf_schema}.cash_flow_statement_annual where ticker = '{sel_tick}' order by year desc")
 
     # metrics for kpi cards
     def kpi_recent(df, metric, periods=2, unit=1000000000):
@@ -167,28 +216,31 @@ with tab2:
     **Example questions to ask:**
 
     - What was the net income in 1996 through 2000 for Proctor and Gamble?
-    - What was the revenue, depreciation, and net cash flows, and total liabilities for Apple for the last 5 years?
+    - What was the revenue vs. total assets for Apple for the last 5 years?
     """
     )
-    conn = st.experimental_connection("snowpark")
-    chain = prompts.load_chain()
+
+    llm = llm_connection(temperature=0, )
     str_input = st.text_input(label='What would you like to answer? (e.g. What was the revenue and net income for Apple for the last 5 years?)')
 
     if len(str_input) > 1:
         with st.spinner('Looking up your question in Snowflake now...'):
-            prompts.execute_chain(str_input)
             try:
-                output = prompts.execute_chain(str_input)
-                try:
-                    st.write(output)
-                    st.write(conn.query(output['result']))
-                except:
-                    st.write("The first attempt didn't pull what you were needing. Trying again...")
-                    output = prompts.execute_chain(f'You need to fix the code. If the question is complex, consider using one or more CTE. Also, examine the DDL statements and try to correct this question/query: {output}')
-                    st.write(output)
-                    st.write(conn.query(output['result']))
+                output = db_chain(str_input)
+                st.write(output['result'])
+                st.dataframe(sf_query(output['intermediate_steps'][1]))
+                #st.write(output)
             except:
                 st.write("Please try to improve your prompt or provide feedback on the error encountered")
+
+        #llm(f"Based on the output provided in {output['result']} produce python code to turn the data into a dataframe and plot the results but skip the import steps")
+        #agent_executor.run(f"Based on the output provided in {output['result']}, create a dataframe of the contents and plot the results with an informative title")
+
+    #agent_executor = create_python_agent(
+    #    llm=llm,
+    #    tool=PythonREPLTool(),
+    #    verbose=False
+    #)
 
     with tab3:
         st.markdown("""
@@ -207,16 +259,26 @@ with tab2:
         """
         )
 
+        pinecone_init()
+        index_name = "buffett"
+        embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["openai_key"])
+
+        docsearch = Pinecone.from_existing_index(index_name,embeddings)
+
+        pdf_qa = ChatVectorDBChain.from_llm(OpenAI(temperature=0.1, model_name="gpt-3.5-turbo",openai_api_key=st.secrets["openai_key"]),
+                            docsearch, return_source_documents=True)
+        
+        @st.cache_data(ttl=600)
+        def pdf_question(query):
+            return pdf_qa({"question": query, "chat_history": ""})
 
         query = st.text_input("What would you like to ask Warren Buffett?")
         if len(query)>1:
             with st.spinner('Looking through lots of Shareholder letters now...'):
-                result = prompts.pdf_question(query)
                 try:
-                    result = prompts.pdf_question(query)
+                    result = pdf_question(query)
                     #print(result)
                     st.write(result['answer'])
-                    st.write(result['source_documents'][0])
                     #st.markdown("Additional details from the search result")
                     #st.write(result)
                 except:
