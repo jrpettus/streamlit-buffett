763ff21ab9ad5df4c1f590ae8d34e47ed6219469
Randy Pettus <jrpettus@gmail.com>
2023-07-20 00:18:42 -0600

    updated db schema

--- buffett_app.py
+++ buffett_app.py
@@ -8,8 +8,8 @@ import prompts
 st.set_page_config(layout="wide")
 
 # Variables
-sf_db = st.secrets["connections.snowpark"]["database"]
-sf_schema = st.secrets["connections.snowpark"]["schema"]
+sf_db = st.secrets["sf_database"]
+sf_schema = st.secrets["sf_schema"]
 tick_list = ['BRK.A','AAPL','PG','JNJ','MA','MCO','VZ','KO','AXP', 'BAC']
 fin_statement_list = ['income_statement','balance_sheet','cash_flow_statement']
 year_cutoff = 20 # year cutoff for financial statement plotting
